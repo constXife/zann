a0d2de682b2516cd200befbda17f7b61c05044a8
constxife <constxife@yandex.ru>
2026-01-10 18:53:33 +0400

    Update nix devshell

--- flake.nix
+++ flake.nix
@@ -13,9 +13,39 @@
     {
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
-          webkitgtk
+          pkg-config
+          openssl
+          libxkbcommon
+          wayland
+          wayland-protocols
+          glib
+          gtk3
+          gdk-pixbuf
+          pango
+          cairo
+          atk
+          libsoup_3
+          webkitgtk_4_1
           xorg.xvfb
+          libayatana-appindicator
         ];
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+          pkgs.openssl
+          pkgs.libxkbcommon
+          pkgs.wayland
+          pkgs.glib
+          pkgs.gtk3
+          pkgs.gdk-pixbuf
+          pkgs.pango
+          pkgs.cairo
+          pkgs.atk
+          pkgs.libsoup_3
+          pkgs.webkitgtk_4_1
+          pkgs.libayatana-appindicator
+        ];
+        OPENSSL_DIR = pkgs.openssl.dev;
+        OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+        OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
       };
     };
 }
